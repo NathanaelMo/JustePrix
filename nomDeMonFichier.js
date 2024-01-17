96710dfe5d7dbb7f8977463efbbc1ea409196a74
unknown <nathanael.monnier@depinfoad.univ-brest.fr>
2024-01-17 08:26:37 +0100

    Création de l'objet UnePerson et aPerson

--- nomDeMonFichier.js
+++ nomDeMonFichier.js
@@ -4,3 +4,22 @@ let b = 45;
 
 b += a ;
 console.log("resultat de a + b = " + b);
+
+let UnePerson = { 
+	name: 'Jean',
+	age: 35,
+	gender: 'masculin',
+	interest: ['musique', 'Jeux videos'],
+};
+
+// on peut également déclarer et initialiser un objet de la façon suivante :
+
+let aPerson= new Object();
+aPerson.name = 'Jean';
+aPerson.age = 35;
+aPerson.gender = 'masculin';
+aPerson.interest = ['musique', 'badminton'];
+
+aPerson.sayHello = function() {console.log('Bonjour ! Je suis ' + this.name + '.'); };
+
+aPerson.sayHello();
