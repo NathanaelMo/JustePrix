2de7344abdb480556d9bfd3ccc7c1d8685a30b3a
nathanael.monnier <nathanael.monnier@depinfoad.univ-brest.fr>
2024-01-17 08:29:03 +0100

    Mise à jour des noms de variables

--- nomDeMonFichier.js
+++ nomDeMonFichier.js
@@ -1,6 +1,7 @@
 console.log('Hello, world!')
-const a = 12;
-let b = 45;
+const operand = 12;
+const operand2 = 45;
 
-b += a ;
-console.log("resultat de a + b = " + b);
+let result = operand + operand2;
+
+console.log("resultat de" + operand + "+" + operand2 + "=" + result);
